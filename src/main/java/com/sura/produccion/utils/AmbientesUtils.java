8719f6ba9b91ae9ea7918cf293261209634e3476
Carlos Acevedo <ceacevedo@sura.com.co>
2018-04-09 10:47:45 -0500

    pruebas unitarias para el proyecto
    Se crea una nueva ruta para pruebas unitarias y se crear pruebas para
    la clase AmbienteUtils.java
    Se modifican algunos features que estan en ingles, ya que el lenguaje
    para los gherkin debe ser ES (español)

--- src/main/java/com/sura/produccion/utils/AmbientesUtils.java
+++ src/main/java/com/sura/produccion/utils/AmbientesUtils.java
@@ -9,18 +9,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AmbientesUtils {
-  final Logger logger = LoggerFactory.getLogger(AmbientesUtils.class);
+  private final Logger logger = LoggerFactory.getLogger(AmbientesUtils.class);
 
   public String getAmbiente() {
-    EnvironmentVariables variables = SystemEnvironmentVariables.createEnvironmentVariables();
-    String envVariable = variables.getProperty("ENV");
+    String envVariable = getEnv();
     logger.info("Ambiente en que corre el proceso. $ENV: " + envVariable);
     String ambienteValidado = ambientesValidos(envVariable);
     return ambienteValidado;
   }
 
+  protected String getEnv() {
+    EnvironmentVariables variables = SystemEnvironmentVariables.createEnvironmentVariables();
+    return variables.getProperty("ENV");
+  }
+
   public String ambientesValidos(String ambiente) {
-    List<String> ambientesValidos = new ArrayList<>(Arrays.asList("local", "dllo", "lab", "pdn"));
+    List<String> ambientesValidos =
+        new ArrayList<String>(Arrays.asList("local", "dllo", "lab", "pdn"));
     if (ambientesValidos.contains(ambiente)) {
       return ambiente;
     } else {
