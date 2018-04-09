984a5e67ae5e4c5ab15ad233d78bc5b9f2127f1f
Carlos Acevedo <ceacevedo@sura.com.co>
2018-04-09 11:31:49 -0500

    ignore out

--- src/unit-test/java/com/sura/produccion/utils/AmbientesUtilsTest.java
+++ src/unit-test/java/com/sura/produccion/utils/AmbientesUtilsTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.Test;
 import org.mockito.Mockito;
 
