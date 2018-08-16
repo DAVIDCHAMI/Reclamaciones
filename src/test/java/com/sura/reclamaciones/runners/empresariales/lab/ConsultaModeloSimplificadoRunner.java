c42d6e91682049a99812ef851fd56e4c800a4ec8
SURAMERICANA\cindreva <crestrepo@qvision.com.co>
2018-08-16 14:41:28 -0500

    Creación de runner

--- src/test/java/com/sura/reclamaciones/runners/empresariales/lab/ConsultaModeloSimplificadoRunner.java
+++ src/test/java/com/sura/reclamaciones/runners/empresariales/lab/ConsultaModeloSimplificadoRunner.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features ="src/test/resources/features/conectar_base_de_datos.feature", glue = {"com.sura.conexion.definitions"})
+@CucumberOptions(features ="src/test/resources/features/modelosimplificado/consulta_modelo_simplificado.feature", glue = {"com.sura.reclamaciones.definitions"})
 public class ConsultaModeloSimplificadoRunner {
 
 }
