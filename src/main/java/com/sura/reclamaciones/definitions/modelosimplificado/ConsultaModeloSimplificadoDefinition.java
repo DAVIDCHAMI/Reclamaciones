9d05cfdc5d793177e7331a79070a6b0dd603dc6b
SURAMERICANA\carldemo <cfex@qvision.com.co>
2018-08-21 15:00:39 -0500

    prueba

--- src/main/java/com/sura/reclamaciones/definitions/modelosimplificado/ConsultaModeloSimplificadoDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/modelosimplificado/ConsultaModeloSimplificadoDefinition.java
@@ -6,40 +6,43 @@ import com.sura.reclamaciones.steps.modelosimplificado.ConsultarModeloSimplifica
 import cucumber.api.java.ast.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import net.thucydides.core.annotations.Steps;
 
 
 public class ConsultaModeloSimplificadoDefinition {
 
-  ModeloSimplificado modeloSimplificado;
-  Connection conexion=null;
-  ResultSet rs;
-  String numeroTransaccion;
-  @Steps private GenericStep genericStep;
-  @Steps ConsultarModeloSimplificadoStep conexionBDSteps;
+    ModeloSimplificado modeloSimplificado;
+    Connection conexion = null;
+    ResultSet rs;
+    String numeroTransaccion;
+    @Steps
+    private GenericStep genericStep;
+    @Steps
+    ConsultarModeloSimplificadoStep conexionBDSteps;
 
-  @Dado("^que se realiza un (.*)$")
-  public void que_se_realiza_un_Pago(String transaccionFinanciera) throws SQLException, IOException {
-    modeloSimplificado = new ModeloSimplificado(genericStep.getFilasModelo("reaseguro_modelo_simplificado", "escenarioCPExcReserva") );
-    conexion = conexionBDSteps.connection();
-  }
+    @Dado("^que se realiza un (.*)$")
+    public void que_se_realiza_un_Pago(String transaccionFinanciera) throws SQLException, IOException {
+        modeloSimplificado = new ModeloSimplificado(genericStep.getFilasModelo("reaseguro_modelo_simplificado", "escenarioCPExcReserva"));
+        conexion = conexionBDSteps.connection();
+    }
 
-  @Cuando("^la transaccion se ha efectuado$")
-  public void la_transaccion_se_ha_efectuado() throws SQLException, IOException {
-    modeloSimplificado.getlstModeloSimplificado();
-    rs = conexionBDSteps.consultar(conexion,modeloSimplificado.getlstModeloSimplificado());
-    //rs = conexionBDSteps.consultar(conexion,numeroTransaccion);
-    System.out.println("Se ejecuto la consulta");
-  }
+    @Cuando("^la transaccion se ha efectuado$")
+    public void la_transaccion_se_ha_efectuado() throws SQLException, IOException {
+        modeloSimplificado.getlstModeloSimplificado();
+        rs = conexionBDSteps.consultar(conexion, modeloSimplificado.getlstModeloSimplificado());
+        //rs = conexionBDSteps.consultar(conexion,numeroTransaccion);
+        System.out.println("Se ejecuto la consulta");
+    }
 
-  @Entonces("^en las fuentes del tablero deben quedar correctos los valores de reaseguro$")
-  public void en_las_fuentes_del_tablero_deben_quedar_correctos_los_valores_de_reaseguro()
-      throws SQLException {
-    conexionBDSteps.verficarConsulta(rs);
-  }
-
+    @Entonces("^en las fuentes del tablero deben quedar correctos los valores de reaseguro$")
+    public void en_las_fuentes_del_tablero_deben_quedar_correctos_los_valores_de_reaseguro()
+            throws SQLException {
+        conexionBDSteps.verficarConsulta(rs);
+    }
 }
