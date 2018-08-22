58fbb41543e4150622f402d1240cb9efbbdab51f
SURAMERICANA\juandigu <jfdiaz@qvision.com.co>
2018-08-22 10:14:58 -0500

    Creación metodo Menú lateral
    Se realiza la creación del método para interactuar con el menú lateral de claims

--- src/main/java/com/sura/reclamaciones/pages/generics/MenuClaimPage.java
+++ src/main/java/com/sura/reclamaciones/pages/generics/MenuClaimPage.java
@@ -1,5 +1,6 @@
 package com.sura.reclamaciones.pages.generics;
 
+import com.sura.reclamaciones.constantes.ConstanteGlobal;
 import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
@@ -12,16 +13,44 @@ public class MenuClaimPage extends GeneralPage {
   WebElementFacade mnuPrimerNivel;
 
   @FindBy(
-    xpath =
-        ".//div[contains(@id,'menu-') and @class='x-panel x-layer x-panel-default x-menu x-border-box']"
+      xpath =
+          ".//div[contains(@id,'menu-') and @class='x-panel x-layer x-panel-default x-menu x-border-box']"
   )
   WebElementFacade mnuSegundoNivel;
 
   @FindBy(xpath = "//input[@id='QuickJump-inputEl']")
   WebElementFacade txtComandoPolicy;
 
+  @FindBy(xpath = " //div[@id='westPanel-innerCt']")
+  WebElementFacade mnuLateralPrimerNivel;
+
+  private String selectOpcion =
+      "//span[contains(@class,'x-tree-node-text')][contains(text(),'COMODIN')]";
+  private String auxSelectOpcion = "";
+
   public MenuClaimPage(WebDriver wDriver) {
     super(wDriver);
+  }
+
+  public void seleccionarOpcionMenuLateralSegundoNivel(String nombreOpcion, String subItem) {
+    mnuLateralPrimerNivel
+        .findElement(
+            By.xpath(
+                "//span[contains(@class,'x-tree-node-text')][contains(text(),'"
+                    + nombreOpcion
+                    + "')]"))
+        .click();
+    realizarEsperaCarga();
+    auxSelectOpcion = selectOpcion.replace(ConstanteGlobal.COMODIN, subItem);
+    $(auxSelectOpcion).waitUntilVisible().click();
+  }
+
+  public void seleecionarOpcionMenuLateralPrimerNivel(String nombreOpcion) {
+    mnuLateralPrimerNivel
+        .findElement(By.xpath("//span[contains(@class,'x-tree-node-text')][contains(text(),'"
+            + nombreOpcion
+            + "')]")).click();
+    realizarEsperaCarga();
   }
 
   public void seleccionarOpcionMenuPrimerNivel(String nombreOpcion) {
