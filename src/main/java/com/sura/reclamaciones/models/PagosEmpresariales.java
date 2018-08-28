518f15d97be8b023c11b9cc67a2d7e2d367ccf4f
CHCORP\grua <grua@choucairtesting.com>
2018-08-28 09:38:20 -0500

    Modificaci{on en el page de  introducir informacion beneficiario, para que consulte por tipo de metodo depago, creaci{on del modelo de datos csv y el archivo

---
+++ src/main/java/com/sura/reclamaciones/models/PagosEmpresariales.java
@@ -0,0 +1,68 @@
+package com.sura.reclamaciones.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ReclamacionEmpresariales extends Reclamacion {
+
+  private List<ReclamacionEmpresariales> lstReclamoEmp = new ArrayList<>();
+  private String numeroContrato;
+  private String detalleHechos;
+  private String pais;
+  private String departamento;
+  private String ciudad;
+  private String direccion;
+
+  public ReclamacionEmpresariales() {
+    super();
+  }
+
+  private ReclamacionEmpresariales(Map<String, String> datosReclamacionEmp) {
+    super(datosReclamacionEmp);
+    this.numeroContrato = datosReclamacionEmp.get("numeroContrato");
+    this.detalleHechos = datosReclamacionEmp.get("detalleHechos");
+    this.pais = datosReclamacionEmp.get("pais");
+    this.departamento = datosReclamacionEmp.get("departamento");
+    this.ciudad = datosReclamacionEmp.get("ciudad");
+    this.direccion = datosReclamacionEmp.get("direccion");
+  }
+
+  public ReclamacionEmpresariales(List<Map<String, String>> datosReclamacionesEmp) {
+    asignarDatos(datosReclamacionesEmp);
+  }
+
+  public String getPais() {
+    return pais;
+  }
+
+  public String getDepartamento() {
+    return departamento;
+  }
+
+  public String getCiudad() {
+    return ciudad;
+  }
+
+  public String getDireccion() {
+    return direccion;
+  }
+
+  public String getNumeroContrato() {
+    return numeroContrato;
+  }
+
+  public String getDetalleHechos() {
+    return detalleHechos;
+  }
+
+  public List<ReclamacionEmpresariales> getLstReclamo() {
+    return lstReclamoEmp;
+  }
+
+  private void asignarDatos(List<Map<String, String>> datosReclamacionesEmp) {
+    for (Map<String, String> dato : datosReclamacionesEmp) {
+      lstReclamoEmp.add(new ReclamacionEmpresariales(dato));
+    }
+  }
+}
