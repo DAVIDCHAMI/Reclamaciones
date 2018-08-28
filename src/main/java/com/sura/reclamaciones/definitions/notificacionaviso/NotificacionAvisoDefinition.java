179583e8466a955d2d5113d1d09db7a1b6438d56
SURAMERICANA\juandigu <jfdiaz@qvision.com.co>
2018-08-28 18:13:49 -0500

    Correción de dependencias
    Se borran dependencias que se incluyeron por efecto de prueba

--- src/main/java/com/sura/reclamaciones/definitions/notificacionaviso/NotificacionAvisoDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/notificacionaviso/NotificacionAvisoDefinition.java
@@ -3,7 +3,6 @@ package com.sura.reclamaciones.definitions.notificacionaviso;
 import com.sura.reclamaciones.models.ReclamacionEmpresariales;
 import com.sura.reclamaciones.steps.generics.GenericStep;
 import com.sura.reclamaciones.steps.notificacionaviso.*;
-import cucumber.api.java.ContinueNextStepsOnException;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
@@ -38,7 +37,6 @@ public class NotificacionAvisoDefinition {
     nuevaReclamacionStep.diligenciarInformacionIncidente(reclamo.getLstReclamo(), tipoIncidente);
   }
 
-  @ContinueNextStepsOnException
   @Entonces("^se obtiene una reclamacion que (.*) genera exposicion$")
   public void verificarExposicion(String exposicion) {
     reclamo.getLstReclamo();
