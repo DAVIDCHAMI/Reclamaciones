8f620a474126ee5f6b6387ff4bc43eb09780d51d
SURAMERICANA\carldemo <cfex@qvision.com.co>
2018-09-05 14:52:53 -0500

    Cambios Modelo Credenciales
    Se borran unos atributos que corresponden a el modelo de BD

--- src/main/java/com/sura/reclamaciones/models/Credencial.java
+++ src/main/java/com/sura/reclamaciones/models/Credencial.java
@@ -6,42 +6,37 @@ import java.util.Map;
 
 public class Credencial {
 
-  private String usuario;
-  private String contrasena;
-  private String urlBaseDatos;
-  private List<Credencial> credenciales = new ArrayList<>();
+    private String usuario;
+    private String contrasena;
+    private List<Credencial> credenciales = new ArrayList<>();
 
-  public Credencial() {}
-
-  private Credencial(Map<String, String> datosUsuario) {
-    this.usuario = datosUsuario.get("usuario");
-    this.contrasena = datosUsuario.get("contrasena");
-    this.urlBaseDatos = datosUsuario.get("urlBaseDatos");
-  }
-
-  public Credencial(List<Map<String, String>> datosUsuario) {
-    asignarDatos(datosUsuario);
-  }
-
-  public String getUsuario() {
-    return usuario;
-  }
-
-  public String getContrasena() {
-    return contrasena;
-  }
-
-  public String getUrlBaseDatos() {
-    return urlBaseDatos;
-  }
-
-  public List<Credencial> getCredenciales() {
-    return credenciales;
-  }
-
-  public void asignarDatos(List<Map<String, String>> datosUsuario) {
-    for (Map<String, String> dato : datosUsuario) {
-      credenciales.add(new Credencial(dato));
+    public Credencial() {
     }
-  }
+
+    private Credencial(Map<String, String> datosUsuario) {
+        this.usuario = datosUsuario.get("usuario");
+        this.contrasena = datosUsuario.get("contrasena");
+    }
+
+    public Credencial(List<Map<String, String>> datosUsuario) {
+        asignarDatos(datosUsuario);
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public List<Credencial> getCredenciales() {
+        return credenciales;
+    }
+
+    public void asignarDatos(List<Map<String, String>> datosUsuario) {
+        for (Map<String, String> dato : datosUsuario) {
+            credenciales.add(new Credencial(dato));
+        }
+    }
 }
