9c65fce2c8bdc1f8db0086a08863ab0ead93a6fe
SURAMERICANA\cindreva <crestrepo@qvision.com.co>
2018-09-11 09:20:44 -0500

    Correcciones
    Se borra el log.1

--- src/main/java/com/sura/reclamaciones/definitions/modelosimplificado/ConsultaModeloSimplificadoDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/modelosimplificado/ConsultaModeloSimplificadoDefinition.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import net.thucydides.core.annotations.Steps;
 
 public class ConsultaModeloSimplificadoDefinition {
-  
+
   String movimientoFinanciero;
 
   @Steps
