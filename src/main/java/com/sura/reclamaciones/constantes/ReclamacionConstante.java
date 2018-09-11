77c1fb4980706d2830e7b35fda2df320181bdfc6
SURAMERICANA\cindreva <crestrepo@qvision.com.co>
2018-09-11 09:58:53 -0500

    Constantes Borradas
    Se borran algunas constantes que estaban en el archivo de ReclamacionConstante

--- src/main/java/com/sura/reclamaciones/constantes/ReclamacionConstante.java
+++ src/main/java/com/sura/reclamaciones/constantes/ReclamacionConstante.java
@@ -1,16 +1,6 @@
 package com.sura.reclamaciones.constantes;
 
 public class ReclamacionConstante {
-  public static final String SUCEDIDO = "¿Qu";
-  public static final String CAUSA_SINIESTRO = "Causa";
-  public static final String ORIGEN_SINIESTRO = "Origen";
-  public static final String AUTORIDAD = "autoridad";
-  public static final String CULPABILIDAD = "Culpabilidad";
-  public static final String PAIS = "Pa";
-  public static final String DEPARTAMENTO = "Departamento";
-  public static final String CIUDAD = "Ciudad";
-  public static final String DIRECCION = "Direcci";
-  public static final String TIPO_POLIZA = "Tipo de";
   public static final String EXPOSICION_CONTENIDO = "Contenido";
   public static final String EXPOSICION_PROPIEDAD = "Propiedad";
   public static final String FECHA_HOY = "Hoy";
