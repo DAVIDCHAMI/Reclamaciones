e1e092950bdaad5a6fe6b05651eaa89d236e7e42
SURAMERICANA\juandigu <jfdiaz@qvision.com.co>
2018-09-13 15:11:53 -0500

    Revert "Eliminación de Recuperos"
    This reverts commit 137bc4a747a1222cec77fcd450dd46da3299ba20.

---
+++ src/main/java/com/sura/reclamaciones/constantes/RecuperoConstante.java
@@ -0,0 +1,9 @@
+package com.sura.reclamaciones.constantes;
+
+public class RecuperoConstante {
+
+  public static final String CANTIDAD = "Cantidad";
+  public static final String CODIGO_RETENCION = "Código de retención";
+  public static final String NUMERO_TRANSACCION = "Número de transacción";
+  public static final String TIPO_TRANSACCION = "Recuperaciones";
+}
