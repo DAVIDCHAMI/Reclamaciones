73a81ff9d9fe4d334b5b95fca3439737cfda972d
liceeset <lestrada@qvision.com.co>
2018-12-05 10:03:25 -0500

    Runner

---
+++ src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote1empresariales/ReaseguroEmpresarialesRunner.java
@@ -0,0 +1,12 @@
+package com.sura.reclamaciones.runners.uat.claimcenter.lote1empresariales;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+    features = "src/test/resources/features/reaseguro/reaseguro_proporcional.feature",
+    glue = {"com.sura.reclamaciones.definitions"}
+)
+public class ReaseguroEmpresarialesRunner {}
