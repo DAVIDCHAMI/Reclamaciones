3e55e10cea04ba25efe52b2495308355f2a168fb
SURAMERICANA\juandigu <jfdiaz@qvision.com.co>
2019-01-21 15:45:54 -0500

    Se realiza ajuste sugerido por revisor

--- src/main/java/com/sura/reclamaciones/pages/generics/GeneralPage.java
+++ src/main/java/com/sura/reclamaciones/pages/generics/GeneralPage.java
@@ -12,7 +12,6 @@ import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.thucydides.core.steps.StepInterceptor;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
