46b695283fa72bc0dcfa8c9c57fda5c6e4edb110
anamilenam <amorales@qvision.com.co>
2019-06-17 16:44:31 -0500

    Se crea la constante ESCRITORIO_MENU ="Escritorio"

--- src/main/java/com/sura/reclamaciones/constantes/MenuConstante.java
+++ src/main/java/com/sura/reclamaciones/constantes/MenuConstante.java
@@ -3,6 +3,7 @@ package com.sura.reclamaciones.constantes;
 public class MenuConstante {
 
   public static final String RECLAMACION_MENU = "Re";
+  public static final String ESCRITORIO_MENU ="Escritorio";
   public static final String NUEVA_RECLAMACION_MENU = "Nueva";
   public static final String RESERVA = "Reserva";
   public static final String DATOS_FINANCIEROS = "Datos financieros";
