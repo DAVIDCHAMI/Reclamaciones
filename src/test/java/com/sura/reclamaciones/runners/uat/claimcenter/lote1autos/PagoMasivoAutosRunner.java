b460ac79a36800b8166e44e1aa3be18df7e6c6e9
anamilenam <amorales@qvision.com.co>
2019-06-17 16:57:18 -0500

    Se crea el Runner

---
+++ src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote1autos/PagoMasivoAutosRunner.java
@@ -0,0 +1,13 @@
+package com.sura.reclamaciones.runners.uat.claimcenter.lote1autos;
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/pagos/crear_pago_masivo.feature",
+        glue = {"com.sura.reclamaciones.definitions"},
+        tags = {"@pagoMasivoProveedor"}
+)
+public class PagoMasivoAutosRunner {
+}
