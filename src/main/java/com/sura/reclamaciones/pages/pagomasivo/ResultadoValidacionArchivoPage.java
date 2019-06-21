476f8ebb60f5b7b1edce44c69ce909733cd87a06
anamilenam <amorales@qvision.com.co>
2019-06-21 15:21:50 -0500

    no message

---
+++ src/main/java/com/sura/reclamaciones/pages/pagomasivo/ResultadoValidacionArchivoPage.java
@@ -0,0 +1,12 @@
+package com.sura.reclamaciones.pages.pagomasivo;
+
+import com.sura.reclamaciones.pages.generics.GeneralPage;
+import org.openqa.selenium.WebDriver;
+
+public class ResultadoValidacionArchivo extends GeneralPage
+{
+    
+    public ResultadoValidacionArchivo(WebDriver wdriver) {
+        super(wdriver);
+    }
+}
