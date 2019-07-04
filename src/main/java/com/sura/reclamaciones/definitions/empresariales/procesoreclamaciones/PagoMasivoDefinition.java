536dfb9e331da3a675b66a9becff8ae327d01468
anamilenam <amorales@qvision.com.co>
2019-07-04 15:21:52 -0500

    Se modifica el feature

--- src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/PagoMasivoDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/PagoMasivoDefinition.java
@@ -7,6 +7,7 @@ import com.sura.reclamaciones.steps.pagomasivo.CargaArchivoPagoMasivoStep;
 import com.sura.reclamaciones.steps.pagomasivo.DetalleFacturaVolumenStep;
 import com.sura.reclamaciones.steps.pagomasivo.ResultadoValidacionArchivoStep;
 import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
 import net.thucydides.core.annotations.Steps;
 
 public class PagoMasivoDefinition {
@@ -20,13 +21,25 @@ public class PagoMasivoDefinition {
 
   @Steps DetalleFacturaVolumenStep detalleFacturaVolumenStep;
 
-  @Cuando("^se crea uno o varios pagos a un mismo proveedor")
-  public void crearPagoMasivo() {
+  @Cuando("^se registra la información de las facturas del pago masivo a un mismo proveedor")
+  public void ingresarInformacionFactura()
+  {
     detalleSiniestroStep.consultarInformacionSiniestro();
-    cargaArchivoPagoMasivoStep.cargarArchivoXls(
-        MenuConstante.ESCRITORIO_MENU, MenuConstante.FACTURAS_VOLUMEN_MENU);
+    cargaArchivoPagoMasivoStep.cargarArchivoXls(MenuConstante.ESCRITORIO_MENU, MenuConstante.FACTURAS_VOLUMEN_MENU);
     resultadoValidacionArchivoStep.validarNumeroRegistrosArchivo();
     resultadoArchivoProcesadoStep.consultarResultadoArchivoProcesado();
     detalleFacturaVolumenStep.consultarBeneficiarioPago();
+  }
+
+  @Cuando("^se ingresa el tipo de proveedor (.*) y el nombre del proveedor (.*) con el tipo de moneda (.*) de la factura y el método de pago (.*) del cheque")
+  public void crearPagoMasivo(String tipoContacto, String contacto, String tipoMoneda, String metodoPago)
+  {
+
+  }
+
+  @Entonces("^se genera un número de pago individual por cada uno de los pagos registrados en el archivo de pagos masivos con un estado de pago solicitado$")
+  public void validarPagoMasivo()
+  {
+
   }
 }
