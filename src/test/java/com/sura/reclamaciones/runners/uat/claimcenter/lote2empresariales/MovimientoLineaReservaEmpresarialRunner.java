daf66d239cb4d0c235d931403a41b433412aceaf
dianccme <dcaicedo@qvision.com.co>
2019-08-28 13:47:51 -0500

    no message

--- src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote2empresariales/MovimientoLineaReservaEmpresarialRunner.java
+++ src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote2empresariales/MovimientoLineaReservaEmpresarialRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-    features = "src/test/resources/features/reservas/movimiento_linea_reserva.feature",
-    glue = {"com.sura.reclamaciones.definitions"},
-    tags = {"@claimsEmpresarial"}
+  features = "src/test/resources/features/reservas/movimiento_linea_reserva.feature",
+  glue = {"com.sura.reclamaciones.definitions"},
+  tags = {"@claimsEmpresarial"}
 )
 public class MovimientoLineaReservaEmpresarialRunner {}
