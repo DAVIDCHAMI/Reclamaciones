1e75a0ab99a77409f969bd041e79c6297d354b35
anamilenam <amorales@qvision.com.co>
2019-09-03 08:55:22 -0500

    no message

--- src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/PagoMasivoDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/PagoMasivoDefinition.java
@@ -11,7 +11,7 @@ import com.sura.reclamaciones.models.*;
 import com.sura.reclamaciones.steps.generics.DetalleSiniestroStep;
 import com.sura.reclamaciones.steps.generics.GenericStep;
 import com.sura.reclamaciones.steps.generics.MenuClaimsStep;
-//import com.sura.reclamaciones.steps.generics.ProcesoBatchStep;
+import com.sura.reclamaciones.steps.generics.ProcesoBatchStep;
 import com.sura.reclamaciones.steps.pagomasivo.*;
 import com.sura.reclamaciones.steps.pagomasivo.CargaArchivoPagoMasivoStep;
 import com.sura.reclamaciones.steps.pagomasivo.ResultadoValidacionArchivoStep;
@@ -39,13 +39,13 @@ public class PagoMasivoDefinition {
 
   @Steps BusquedaLibretaContactoStep busquedaLibretaContactoStep;
 
-  //@Steps ProcesoBatchStep procesoBatchStep;
+  @Steps ProcesoBatchStep procesoBatchStep;
 
-  //@Steps FacturaVolumenStep facturaVolumenStep;
+  @Steps FacturaVolumenStep facturaVolumenStep;
 
   @Steps MenuClaimsStep menuClaimsStep;
 
-  //@Steps DatoFinancieroPagoStep datoFinancieroPagoStep;
+  @Steps DatoFinancieroPagoStep datoFinancieroPagoStep;
 
   @Steps NuevoPagoStep nuevoPagoStep;
 
@@ -62,8 +62,8 @@ public class PagoMasivoDefinition {
     nuevoPagoStep.crearExposicionVehicularManual(
             genericStep.getFilasModelo(
                     PARAMETROS_NAVEGACION_MENU_ACCIONES.getValor(), EXPOSICION_MANUAL_VEHICULAR.getValor()),
-            exposicionVehiculoTercero.getLstExposicionTerceros());
-    detalleSiniestroStep.consultarInformacionSiniestro();*/
+            exposicionVehiculoTercero.getLstExposicionTerceros());*/
+    detalleSiniestroStep.consultarInformacionSiniestro();
     /*Exposicion datosExposicionPagoMasivo =
             new Exposicion(
                     genericStep.getFilasModelo(
@@ -88,19 +88,19 @@ public class PagoMasivoDefinition {
       String tipoContacto, String contacto, String tipoMoneda, String metodoPago) {
     detalleFacturaVolumenStep.ingresarInformacionFactura(tipoMoneda, metodoPago);
     busquedaLibretaContactoStep.buscarContactoPagoMasivo(tipoContacto, contacto);
-    //detalleFacturaVolumenStep.crearPagoMasivo();
-    //procesoBatchStep.ejecutarProcesoBatch();
+    detalleFacturaVolumenStep.crearPagoMasivo();
+    procesoBatchStep.ejecutarProcesoBatch();
   }
 
   @Entonces(
       "^se genera un número de pago individual por cada uno de los pagos registrados en el archivo de pagos masivos con un estado de pago solicitado$")
   public void validarPagoMasivo() {
-    /*facturaVolumenStep.buscarNumeroFacturaPagoMasivo(
+    facturaVolumenStep.buscarNumeroFacturaPagoMasivo(
         MenuConstante.ESCRITORIO_MENU, MenuConstante.FACTURAS_VOLUMEN_MENU);
     detalleFacturaVolumenStep.validarPagoMasivo();
     menuClaimsStep.consultarNumeroReclamacion(
         Serenity.sessionVariableCalled(SESION_CC_NUMERO_SINIESTRO.getValor()));
     datoFinancieroPagoStep.validarPagosIndividuales(
-        MenuConstante.DATOS_FINANCIEROS, MenuConstante.PAGOS);*/
+        MenuConstante.DATOS_FINANCIEROS, MenuConstante.PAGOS);
   }
 }
--- src/main/java/com/sura/reclamaciones/steps/generics/ProcesoBatchStep.java
+++ src/main/java/com/sura/reclamaciones/steps/generics/ProcesoBatchStep.java
@@ -1,6 +1,9 @@
 package com.sura.reclamaciones.steps.generics;
 
 import static com.sura.reclamaciones.constantes.MenuConstante.VOLVER_CLAIMCENTER_MENU;
+import static com.sura.reclamaciones.constantes.NombreProcesoBatch.ENVIO_FACTURA_VOLUMEN;
+import static com.sura.reclamaciones.constantes.NombreProcesoBatch.MONITOR_FLUJO_TRABAJO_FACTURA_VOLUMEN;
+import static com.sura.reclamaciones.constantes.NombreProcesoBatch.TRANSFERENCIA_FACTURA_VOLUMEN;
 
 import com.sura.reclamaciones.pages.generics.ProcesoBatchPage;
 import org.fluentlenium.core.annotation.Page;
@@ -11,9 +14,9 @@ public class ProcesoBatchStep {
 
   public void ejecutarProcesoBatch() {
     procesoBatchPage.ejecutarBatch();
-    /*procesoBatchPage.ejecutarProcesoBatch(ENVIO_FACTURA_VOLUMEN.getValor());
+    procesoBatchPage.ejecutarProcesoBatch(ENVIO_FACTURA_VOLUMEN.getValor());
     procesoBatchPage.ejecutarProcesoBatch(MONITOR_FLUJO_TRABAJO_FACTURA_VOLUMEN.getValor());
-    procesoBatchPage.ejecutarProcesoBatch(TRANSFERENCIA_FACTURA_VOLUMEN.getValor());*/
+    procesoBatchPage.ejecutarProcesoBatch(TRANSFERENCIA_FACTURA_VOLUMEN.getValor());
     procesoBatchPage.seleccionarOpcionMenuAccion();
     procesoBatchPage.seleccionarOpcionMenuAccionesPrimerNivel(VOLVER_CLAIMCENTER_MENU);
   }
