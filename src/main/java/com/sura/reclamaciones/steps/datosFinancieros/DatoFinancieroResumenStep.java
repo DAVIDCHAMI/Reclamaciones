533fa0261f6892022e2b63f56d61a9426eb2aba2
elizabethcorrea <elcorreai@qvision.com.co>
2019-09-04 11:40:17 -0500

    Cambio y modificación de archivos

---
+++ src/main/java/com/sura/reclamaciones/steps/datosFinancieros/DatoFinancieroResumenStep.java
@@ -0,0 +1,29 @@
+package com.sura.reclamaciones.steps.datosFinancieros;
+
+import com.sura.reclamaciones.models.Reserva;
+import com.sura.reclamaciones.pages.datosfinancieros.DatoFinancieroResumenPage;
+import com.sura.reclamaciones.pages.generics.MenuClaimPage;
+import net.thucydides.core.annotations.Step;
+import org.fluentlenium.core.annotation.Page;
+import org.hamcrest.MatcherAssert;
+
+import java.util.List;
+
+import static com.sura.reclamaciones.constantes.Constantes.DATOS_FINANCIEROS;
+
+public class DatoFinancieroResumenStep {
+
+    @Page
+    MenuClaimPage menuClaimPage;
+
+    @Page
+    DatoFinancieroResumenPage datoFinancieroResumenPage;
+
+    @Step
+    public void validarValorReservas(List<Reserva> lineaReserva) {
+        menuClaimPage.seleccionarOpcionMenuLateralPrimerNivel(DATOS_FINANCIEROS.getValor());
+        boolean valorLineaReserva = datoFinancieroResumenPage.obtenerDatosFinancieros(lineaReserva);
+        MatcherAssert.assertThat(
+                "No coinciden todos los valores de las líneas de reserva", valorLineaReserva);
+    }
+}
