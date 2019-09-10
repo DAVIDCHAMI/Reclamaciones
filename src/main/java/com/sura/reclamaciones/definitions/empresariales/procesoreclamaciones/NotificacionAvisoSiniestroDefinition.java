db6f5fe7a0c6545e6fd087597a98c3f10fdc8c38
Cecilia <chechi18c@gmail.com>
2019-09-10 11:30:15 -0500

    actualizacion PR

--- src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/NotificacionAvisoSiniestroDefinition.java
+++ src/main/java/com/sura/reclamaciones/definitions/empresariales/procesoreclamaciones/NotificacionAvisoSiniestroDefinition.java
@@ -1,12 +1,11 @@
 package com.sura.reclamaciones.definitions.empresariales.procesoreclamaciones;
 
-import static com.sura.reclamaciones.utils.UtilidadesCSV.*;
+import static com.sura.reclamaciones.utils.UtilidadesCSV.obtenerDatosPrueba;
 import static com.sura.reclamaciones.utils.VariablesSesion.SESION_CC_TIPO_PRODUCTO_EMPRESARIAL;
 
 import com.sura.reclamaciones.constantes.MenuConstante;
 import com.sura.reclamaciones.constantes.ReclamacionConstante;
 import com.sura.reclamaciones.models.ReclamacionEmpresarial;
-import com.sura.reclamaciones.steps.generics.GenericStep;
 import com.sura.reclamaciones.steps.notificacionaviso.NuevaReclamacionEmpresarialStep;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
@@ -20,8 +19,6 @@ public class NotificacionAvisoSiniestroDefinition {
   @Steps ReclamacionEmpresarial reclamacionEmpresarial;
 
   @Steps NuevaReclamacionEmpresarialStep nuevaReclamacionEmpresarialStep;
-
-  @Steps GenericStep genericStep;
 
   @Dado("^que se tiene una póliza de (.*)$")
   public void buscarPoliza(String tipoCobertura) throws IOException {
