f96d400a82a4122eac5810fa53289b8652fd6822
anamilenam <amorales@qvision.com.co>
2019-10-10 09:10:35 -0500

    Se renombra runner

--- src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote1autos/ChequeMultiplesPagosAutosRunner.java
+++ src/test/java/com/sura/reclamaciones/runners/uat/claimcenter/lote1autos/ChequeMultiplesPagosAutosRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
   features = "src/test/resources/features/pagos/pagar_siniestro.feature",
   glue = {"com.sura.reclamaciones.definitions"},
-  tags = {"@pagoLineaReservaAutos"}
+  tags = {"@chequeMultiplesPagosAutos"}
 )
-public class ChequeMultipagosAutosRunner {}
+public class ChequeMultiplesPagosAutosRunner {}
