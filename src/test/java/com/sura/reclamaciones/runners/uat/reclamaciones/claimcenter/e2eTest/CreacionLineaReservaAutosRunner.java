da905439d25f7443c15461c3ae4ed85a4119e18f
DAVID_MORENO_M <chamirojo.16@hotmail.es>
2021-07-29 12:07:17 -0500

    se ajusta encarpetado para las clases runner

--- src/test/java/com/sura/reclamaciones/runners/uat/reclamaciones/claimcenter/e2eTest/CreacionLineaReservaAutosRunner.java
+++ src/test/java/com/sura/reclamaciones/runners/uat/reclamaciones/claimcenter/e2eTest/CreacionLineaReservaAutosRunner.java
@@ -1,4 +1,4 @@
-package com.sura.reclamaciones.runners.uat.reclamaciones.claimcenter.integrationTest;
+package com.sura.reclamaciones.runners.uat.reclamaciones.claimcenter.e2eTest;
 
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-    features = "src/test/resources/features/autos/reservas/reversar_constitucion.feature",
+    features = "src/test/resources/features/autos/reservas/crear_linea_reserva.feature",
     glue = {"com.sura.reclamaciones.definitions"},
     tags = {"@claimsAuto"})
-public class ReversionConstitucionAutosRunner {}
+public class CreacionLineaReservaAutosRunner {}
