9c35c85a50140b3e93893479a612f66791ae54b4
DAVID_MORENO_M <chamirojo.16@hotmail.es>
2021-12-20 15:02:56 -0500

    se realizan ajustes en tags y time out

--- src/test/java/com/sura/reclamaciones/runners/uat/reclamaciones/claimcenter/e2eTest/RecuperoSiniestroAutosRunner.java
+++ src/test/java/com/sura/reclamaciones/runners/uat/reclamaciones/claimcenter/e2eTest/RecuperoSiniestroAutosRunner.java
@@ -8,5 +8,5 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
     features = "src/test/resources/features/autos/recuperos/crear_recupero_autos.feature",
     glue = {"com.sura.reclamaciones.definitions"},
-    tags = {"@Pending"})
+    tags = {"@claimsAuto"})
 public class RecuperoSiniestroAutosRunner {}
